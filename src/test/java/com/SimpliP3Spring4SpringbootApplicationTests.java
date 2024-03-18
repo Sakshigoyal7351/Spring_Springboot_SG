90428e29a717cbbd5dfa929ea5e0ef29f7abb775
sakshi goyal <sakshigoyal7351@gmail.com>
2024-03-18 17:04:24 +0530

    first commit

---
+++ src/test/java/com/SimpliP3Spring4SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpliP3Spring4SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
